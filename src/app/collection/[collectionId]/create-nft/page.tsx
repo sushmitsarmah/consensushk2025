5584ba55047c70d7fff15f63d2eb9525cc0f65b7
sushmitsarmah <sushmitsarmah@gmail.com>
2025-02-19 18:08:15 +0800

    fixed build

--- src/app/collection/[collectionId]/create-nft/page.tsx
+++ src/app/collection/[collectionId]/create-nft/page.tsx
@@ -1,10 +1,16 @@
-import { CreateNFTForm } from "@/components/elems/create-nft-form"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client"
 
-export default function CreateNFTPage({ params }: { params: { collectionId: string } }) {
+import { CreateNFTForm } from "@/components/elems/create-nft-form"
+import { use } from "react"
+
+export default function CreateNFTPage({ params }: any) {
+  const { collectionId } = use<any>(params)
+
   return (
     <div className="max-w-3xl mx-auto">
       <h1 className="text-3xl font-bold text-slate-900 mb-8">Create NFT</h1>
-      <CreateNFTForm collectionId={params.collectionId} />
+      <CreateNFTForm collectionId={collectionId} />
     </div>
   )
 }
